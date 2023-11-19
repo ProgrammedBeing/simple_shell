dedb423697c0c9d719453f1c34809293448468cc
Emmanuel Onyekachi <kachirhaps@gmail.com>
2023-11-19 02:55:21 -0800

    Added

---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdio.h>
+void execute_command(char *command);
+#endif
