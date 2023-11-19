bf217fa9dfa7026ab63e25a43ed51a716e7d6e90
Emmanuel Onyekachi <kachirhaps@gmail.com>
2023-11-19 02:53:38 -0800

    Added file

--- custom_shell.c
+++ custom_shell.c
@@ -1,4 +1,32 @@
 #include "main.h"
+/**
+ * execute_command - excutes command
+ * @command: Command.
+ * Return: Return nothing.
+ */
+void execute_command(char *command)
+{
+	int status;
+	pid_t pid = fork();
+
+	if (pid < 0)
+	{
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execlp(command, command, NULL) == -1)
+		{
+			printf("%s: No such file or directory\n", command);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+}
 
 /**
  * main- ENtry point.
@@ -7,48 +35,23 @@
  */
 int main(void)
 {
-	pid_t pid;
 	char *command = NULL;
 	size_t len = 0;
 	ssize_t nread;
-	int status;
 
 	while (1)
 	{
 		printf("#cisfun$ ");
-
 		nread = getline(&command, &len, stdin);
-
 		if (nread <= 0)
 		{
 			printf("\n");
 			break;
 		}
-
 		if (command[nread - 1] == '\n')
 			command[nread - 1] = '\0';
-
-		pid = fork();
-
-		if (pid < 0)
-		{
-			perror("Fork failed");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			if (execlp(command, command, NULL) == -1)
-			{
-				printf("%s: No such file or directory\n", command);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-		}
+		execute_command(command);
 	}
-
 	free(command);
 	return (0);
 }
